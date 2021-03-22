ae86172b3cca9cc9e8c944ece9c1b7e3464508de
Laura <G00398298@gmit.ie>
2021-03-22 23:13:33 +0000

    specified font style, added a background grid, changed line style, added axis names and titles, added legend

--- plottask.py
+++ plottask.py
@@ -8,6 +8,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# specify what font style to use for all text in the plot
+plt.rcParams["font.family"] = "Courier New"
+
 # define the range of values for x on the graph
 x = np.arange(0, 4)
 # define the values of the functions to be plotted
@@ -15,12 +18,23 @@ f = x
 g = x**2
 h = x**3
 
+# display a grid and specify its style
+plt.rc('grid', linestyle="dotted", color='gray', alpha=0.7)
+plt.grid()
+
 # call the plot command, individually for each line to be plotted
-# I used the tableau color blind 10 palette to choose colours for my lines
-# converted them from rgb to hex for easier entry
-plt.plot(f, color = '#006ba4', linewidth=3.0)
-plt.plot(g, color = '#ff800e', linewidth=3.0)
-plt.plot(h, color = '#595959', linewidth=3.0)  
+# specify the color, style of, width and label for each line
+plt.plot(f, color = '#006ba4', linestyle='solid', linewidth=3.0, label='f(x)')
+plt.plot(g, color = '#ff800e', linestyle='dashed', dashes=(4, 2), linewidth=3.0, label='g(x)')
+plt.plot(h, color = '#595959', linestyle='dashed', dashes=(2, 1),linewidth=3.0, label='h(x)')  
+
+# specify the name for each axis and for the title
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.title("plot of f(x), g(x) and h(x)")
+# display a legend
+plt.legend()
+
 # show command will display the resulting graph
 plt.show()
 
@@ -33,3 +47,9 @@ plt.show()
 # https://www.rgbtohex.net/rgb/ (accessed 20/03/2021)
 # https://public.tableau.com/profile/chris.gerrard#!/vizhome/TableauColors/ColorPaletteswithRGBValues (accessed 20/03/2021)
 # https://matplotlib.org/stable/gallery/index.html (accessed 20/03/2021)
+
+# https://matplotlib.org/stable/gallery/lines_bars_and_markers/line_demo_dash_control.html (accessed 22/03/2021)
+# https://stackoverflow.com/questions/43814540/how-to-change-the-font-of-the-legend (accessed 22/03/2021)
+# https://pythonbasics.org/matplotlib-line-chart/ (accessed 22/03/2021)
+# https://jakevdp.github.io/PythonDataScienceHandbook/04.01-simple-line-plots.html (accessed 22/03/2021)
+# https://stackoverflow.com/questions/8209568/how-do-i-draw-a-grid-onto-a-plot-in-python (acessed 22/03/2011)
