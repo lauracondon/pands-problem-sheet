abf741171a7a0b4ed6126f0caef4beba68bc9805
Laura <G00398298@gmit.ie>
2021-04-01 19:41:04 +0100

    edited and added more code comments. removed references

--- squareroot.py
+++ squareroot.py
@@ -7,24 +7,23 @@
 def sqrt(x):
         # finds the approximate square root of the input
         approx = 0.5 * x
-        # uses the Newton Method to find a better approximation of the square root
+        # uses Newton's Method to find a better approximation of the square root
         better = 0.5 * (approx + x/approx)
-        # repeat the Newton Method until the approx. square root no longer changes
+        # repeats Newton's Method until the approx. square root no longer changes
         while better != approx:
             approx = better
             better = 0.5 * (approx + x/approx)
         return approx
 
+
+# requests input from the user
 num =  float(input("Please enter a positve number: "))
+# throws an error message if they enter a negative number
 if num < 0:
     print("That was a negative number! Please try again")
 else:
-    #call the function sqrt
+    # calls the function sqrt
     sqrtNum = (sqrt(num))
+    # prints the output rounded to one decimal place
     print("The square root of " + str(num) + " is approx. " + str(round(sqrtNum,1)))
 
-# References: 
-# Newton Method - https://runestone.academy/runestone/books/published/thinkcspy/MoreAboutIteration/NewtonsMethod.html
-# accessed 28/02/2021
-# Limiting to one decimal place - https://stackoverflow.com/questions/20457038/how-to-round-to-2-decimals-with-python
-# accessed 28/02/2021
