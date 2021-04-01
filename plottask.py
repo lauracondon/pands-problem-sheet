f9dedd837012e2fb4f6e2f11161a57e445056d21
Laura <G00398298@gmit.ie>
2021-04-01 21:57:51 +0100

    edited comments and removed references

--- plottask.py
+++ plottask.py
@@ -4,52 +4,37 @@
 # Author: Laura Condon
 
 # import the required modules - numpy and matplotlib
-# Use 'as' to shorten how you refer to them in the program
+# Use 'as' to shorten how they are called in the program
 import numpy as np
 import matplotlib.pyplot as plt
 
-# specify what font style to use for all text in the plot
+# specifies what font style to use for all text in the plot
 plt.rcParams['font.family'] = 'Courier New'
 
-# define the range of values for x on the graph
+# defines the range of values for x on the graph
 x = np.arange(0, 4)
-# define the values of the functions to be plotted
+# defines the values of the functions to be plotted
 f = x 
 g = x**2
 h = x**3
 
-# display a grid and specify its style
+# displays a grid and specifies its style
 plt.rc('grid', linestyle='dotted', color='gray', alpha=0.7)
 plt.grid()
 
-# call the plot command, individually for each line to be plotted
-# specify the color, style of, width and label for each line
+# calls the plot command, individually for each line to be plotted
+# specifies the color, style of, width and label of each line
 plt.plot(f, color = '#006ba4', linestyle='solid', linewidth=3.0, label='f(x)')
 plt.plot(g, color = '#ff800e', linestyle='dashed', dashes=(4, 2), linewidth=3.0, label='g(x)')
 plt.plot(h, color = '#595959', linestyle='dashed', dashes=(2, 1),linewidth=3.0, label='h(x)')  
 
-# specify the name for each axis and for the title
+# specifies the name for each axis and for the title
 plt.xlabel('x axis')
 plt.ylabel('y axis')
 plt.title('plot of f(x), g(x) and h(x)')
-# display a legend
+# displays a legend
 plt.legend()
 
-# show command will display the resulting graph
+# displays the resulting graph
 plt.show()
 
-#References: 
-# https://realpython.com/tutorials/numpy/ (accessed 10/03/2021)
-# https://realpython.com/how-to-use-numpy-arange/ (accessed 10/03/2021)
-# https://www.tutorialspoint.com/matlab/matlab_plotting.htm (accessed 10/03/2021)
-# https://www.kite.com/python/answers/how-to-plot-multiple-lines-on-the-same-graph-in-matplotlib-in-python(accessed 10/03/2021)
-
-# https://www.rgbtohex.net/rgb/ (accessed 20/03/2021)
-# https://public.tableau.com/profile/chris.gerrard#!/vizhome/TableauColors/ColorPaletteswithRGBValues (accessed 20/03/2021)
-# https://matplotlib.org/stable/gallery/index.html (accessed 20/03/2021)
-
-# https://matplotlib.org/stable/gallery/lines_bars_and_markers/line_demo_dash_control.html (accessed 22/03/2021)
-# https://stackoverflow.com/questions/43814540/how-to-change-the-font-of-the-legend (accessed 22/03/2021)
-# https://pythonbasics.org/matplotlib-line-chart/ (accessed 22/03/2021)
-# https://jakevdp.github.io/PythonDataScienceHandbook/04.01-simple-line-plots.html (accessed 22/03/2021)
-# https://stackoverflow.com/questions/8209568/how-do-i-draw-a-grid-onto-a-plot-in-python (acessed 22/03/2011)
