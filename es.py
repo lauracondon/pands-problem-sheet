f833e808fda034d0fd6f6b218a9c4c63d11bf884
Laura <G00398298@gmit.ie>
2021-04-01 21:02:16 +0100

    changed naming convention + deleted references

--- es.py
+++ es.py
@@ -4,24 +4,16 @@
 # Author: Laura Condon
 
 # takes the filename from an argument on the command line
-textFile = input("Please enter the name of your file: ")
+text_file = input("Please enter the name of your file: ")
 # the file is opened as openFile in read mode, encoding is specified to ensure no errors when reading the file
-with open(textFile, "r", encoding="utf8") as openFile:
-    textString = openFile.read()
+with open(text_file, "r", encoding="utf8") as open_file:
+    text_string = open_file.read()
     # checks for lowercase e
-    numOfE = textString.count("e")
+    num_of_e = text_string.count("e")
     # checks for uppercase e
-    numOfCapE = textString.count("E")
+    num_of_cap_e = text_string.count("E")
     # adds the two resulting integers together
-    totalE = numOfE + numOfCapE
+    total_e = num_of_e + num_of_cap_e
     # prints the result
-    print("The letter e appears in this document " + str(totalE) + " times")
+    print("The letter e appears in this document " + str(total_e) + " times")
 
-
-    # https://realpython.com/read-write-files-python/ (accessed 4th March 2021)
-    # https://realpython.com/working-with-files-in-python/ (accessed 4th March 2021)
-    # https://www.w3schools.com/python/python_file_handling.asp (accessed 4th March 2021)
-    # https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
-    # https://www.codegrepper.com/code-examples/python/how+to+count+the+occurrence+of+a+word+in+string+python (accessed 4th March 2021)
-    # https://stackoverflow.com/questions/9233027/unicodedecodeerror-charmap-codec-cant-decode-byte-x-in-position-y-character (accessed 4th March 2021)
-    # https://www.gutenberg.org/files/2701/2701-h/2701-h.htm (accessed 4th March 2021)
