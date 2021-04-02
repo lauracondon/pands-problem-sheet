3a38fbf4c9a591b5b9989ad599d3d5f8fbd8ed4c
Laura <G00398298@gmit.ie>
2021-04-02 22:09:43 +0100

    removed r from open, added 'for line' line 12

--- es.py
+++ es.py
@@ -5,15 +5,18 @@
 
 # takes the filename from an argument on the command line
 text_file = input("Please enter the name of your file: ")
-# the file is opened as openFile in read mode, encoding is specified to ensure no errors when reading the file
-with open(text_file, "r", encoding="utf8") as open_file:
-    text_string = open_file.read()
-    # checks for lowercase e
-    num_of_e = text_string.count("e")
-    # checks for uppercase e
-    num_of_cap_e = text_string.count("E")
-    # adds the two resulting integers together
-    total_e = num_of_e + num_of_cap_e
-    # prints the result
-    print("The letter e appears in this document " + str(total_e) + " times")
+# the file is opened as open_file in read mode, encoding is specified to ensure no errors when reading the file
+# as read or 'r' is the default we don't need to specify it here
+with open(text_file, encoding="utf8") as open_file:
+    # this checks the file one line at a time
+    for line in open_file:
+        text_string = open_file.read()
+            # checks for lowercase e
+        num_of_e = text_string.count("e")
+            # checks for uppercase e
+        num_of_cap_e = text_string.count("E")
+            # adds the two resulting integers together
+        total_e = num_of_e + num_of_cap_e
+            # prints the result
+        print("The letter e appears in this document " + str(total_e) + " times")
 
