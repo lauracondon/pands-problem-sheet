65020c7657aaca703e520f1d71ba84b6bc8c5f71
Laura <G00398298@gmit.ie>
2021-04-16 21:03:03 +0100

    edited formatting and code comments

--- plottask.py
+++ plottask.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # specifies what font style to use for all text in the plot
-plt.rcParams['font.family'] = 'DejaVu Sans'
+plt.rcParams["font.family"] = 'DejaVu Sans'
 
 # defines the range of values for x on the graph
 x = np.arange(0, 4)
@@ -19,24 +19,24 @@ g = x**2
 h = x**3
 
 # displays a grid and specifies its style
-plt.rc('grid', linestyle='dotted', color='gray', alpha=0.7)
+plt.rc("grid", linestyle = "dotted", color = "gray", alpha=0.7)
 plt.grid()
 
 # calls the plot command, individually for each line to be plotted
-# specifies the color, style of, width and label of each line
-plt.plot(f, color = '#006ba4', linestyle='solid', linewidth=3.0, label='f(x)')
-plt.plot(g, color = '#ff800e', linestyle='dashed', dashes=(4, 2), linewidth=3.0, label='g(x)')
-plt.plot(h, color = '#595959', linestyle='dashed', dashes=(2, 1),linewidth=3.0, label='h(x)')  
+# the style of each line is specified using a variety of attributes
+plt.plot(f, color = "#006ba4", linestyle = "solid", linewidth = 3.0, label= "f(x)")
+plt.plot(g, color = "#ff800e", linestyle = "dashed", dashes = (4, 2), linewidth = 3.0, label= "g(x)")
+plt.plot(h, color = "#595959", linestyle = "dashed", dashes = (2, 1),linewidth = 3.0, label= "h(x)")  
 
 # specifies the name for each axis and for the title
-plt.xlabel('x axis')
-plt.ylabel('y axis')
-plt.title('plot of f(x), g(x) and h(x)')
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.title("plot of f(x), g(x) and h(x)")
 # displays a legend
 plt.legend()
 
 # saves graph to plottask.png
-plt.savefig('plottask.png')
+plt.savefig("plottask.png")
 # displays the resulting graph
 plt.show()
 
